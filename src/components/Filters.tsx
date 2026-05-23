7eaee2e2a0b5f6f912b6d326b724c4e57da271db
Dominik Loncar <domba96@gmail.com>
2026-05-22 21:02:41 -0400

    Remove status filter

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,44 +1,17 @@
-import type { EventStatus } from '../types/Event';
-
 export interface FilterState {
-  status: 'all' | EventStatus;
   attendee: 'all' | string;
 }
 
 interface FiltersProps {
   filters: FilterState;
-  statuses: EventStatus[];
   attendees: string[];
   onChange: (filters: FilterState) => void;
 }
 
-const labelFor = (value: string) =>
-  value
-    .split('-')
-    .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
-    .join(' ');
-
-export function Filters({ filters, statuses, attendees, onChange }: FiltersProps) {
+export function Filters({ filters, attendees, onChange }: FiltersProps) {
   return (
     <section className="filters" aria-label="Event filters">
       <div className="filter-selects">
-        <label>
-          <span>Status</span>
-          <select
-            value={filters.status}
-            onChange={(event) =>
-              onChange({ ...filters, status: event.target.value as FilterState['status'] })
-            }
-          >
-            <option value="all">All statuses</option>
-            {statuses.map((status) => (
-              <option value={status} key={status}>
-                {labelFor(status)}
-              </option>
-            ))}
-          </select>
-        </label>
-
         <label>
           <span>Friend</span>
           <select
